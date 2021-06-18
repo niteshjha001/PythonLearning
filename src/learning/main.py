5f640b34a953d0ccd4703d3423355b8e513240c1
niteshjha001 <nitesh.jha001@mymdc.net>
2021-06-18 15:37:33 +0000

    extensive library

--- src/learning/main.py
+++ src/learning/main.py
@@ -1,4 +1,12 @@
-x=100
-def f1():
-    print("how are you:",x)
-f1()
+from random import*
+for i in range(10):
+       print(randint(0,9),randint(0,9),randint(0,9),randint(0,9),sep='')
+
+
+
+
+
+
+
+
+

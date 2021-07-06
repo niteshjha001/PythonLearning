4a2287d30d989b54820d7ee35d2932b8f840faa3
niteshjha001 <nitesh.jha001@mymdc.net>
2021-07-06 08:32:40 +0000

    new change

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,7 +1,11 @@
-x=0b1111+20j
-print(x)
-print(x.real,x.imag)
+#x=0b1111+20j
+#print(x)
+#print(x.real,x.imag)
 
-#Bool#
-b=True/True
-print(b)
+#Bool
+#b=True/False
+#print(b) #zero division error#
+
+#slice operator#
+s='Himanshu'
+print(s[-5])

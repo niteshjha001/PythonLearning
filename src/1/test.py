f6c62e01ed590bec6f581034140f6e2250e3b59f
niteshjha001 <nitesh.jha001@mymdc.net>
2021-08-13 16:16:25 +0000

    operator in python

--- src/1/test.py
+++ src/1/test.py
@@ -1,1 +1,10 @@
-print(s)
+a=20.0
+b=2.5
+print('a+b=',a+b)#addtion12
+print('a-b=',a-b)#subtract
+print('a*b=',a*b)#multiplication
+print('a/b=',a/b)#division
+print('a%b=',a%b)#modulo
+print('a//b=',a//b)#floor division
+print('a**b=',a**b)#exponent operator
+print('himanshu'+'nitesh')#string concatenate operator
